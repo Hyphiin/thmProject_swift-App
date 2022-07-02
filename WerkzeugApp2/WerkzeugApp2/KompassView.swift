b42ffa79bf302db2e03e71c36274acbf34966a72
Chiara Knipprath <chiara.knipprath@thm.iem.de>
2022-07-02 16:20:01 +0200

    Compass Style

--- WerkzeugApp2/WerkzeugApp2/KompassView.swift
+++ WerkzeugApp2/WerkzeugApp2/KompassView.swift
@@ -10,8 +10,87 @@ import SwiftUI
 struct KompassView: View {
     var body: some View {
         VStack{
-            
+            Capsule()
+                .frame(width: 5, height: 50)
+            ZStack{
+                ForEach(Marker.markers(), id:\.self) {
+                    marker in CompassMarkerView(marker: marker, compassDegrees: 0)
+                }
+            }
+            .frame(width: 300, height: 300)
+            .rotationEffect(Angle(degrees: 0))
+            .statusBar(hidden: true)
         }
         .navigationTitle("Kompass")
     }
+}
+
+struct Marker: Hashable {
+    let degrees: Double
+    let lable: String
+    
+    init(degrees: Double, lable: String = ""){
+        self.degrees = degrees
+        self.lable = lable
+    }
+    
+    static func markers() -> [Marker] {
+        return [
+            Marker(degrees: 0, lable: "N"),
+            Marker(degrees: 30, lable: ""),
+            Marker(degrees: 60, lable: ""),
+            Marker(degrees: 90, lable: "E"),
+            Marker(degrees: 120, lable: ""),
+            Marker(degrees: 150, lable: ""),
+            Marker(degrees: 180, lable: "S"),
+            Marker(degrees: 210, lable: ""),
+            Marker(degrees: 240, lable: ""),
+            Marker(degrees: 270, lable: "W"),
+            Marker(degrees: 300, lable: ""),
+            Marker(degrees: 330, lable: ""),
+        ]
+    }
+    
+    func degreeText() -> String {
+        return String(format: "%.0f", self.degrees)
+    }
+}
+
+struct CompassMarkerView: View {
+    let marker: Marker
+    let compassDegrees: Double
+    
+    var body: some View {
+        VStack {
+            Text(marker.degreeText())
+                .fontWeight(.light)
+                .rotationEffect(self.textAngle())
+            Capsule()
+                .frame(width: self.capsuleWidth(), height: self.capsuleHeight())
+                .foregroundColor(self.capsuleColor())
+                .padding(.bottom, 120)
+            Text(marker.lable)
+                .fontWeight(.bold)
+                .rotationEffect(self.textAngle())
+                .padding(.bottom, 80)
+        }
+        .rotationEffect(Angle(degrees: marker.degrees))
+    }
+    
+    private func capsuleWidth() -> CGFloat {
+        return self.marker.degrees == 0 ? 7 : 3
+    }
+    
+    private func capsuleHeight() -> CGFloat {
+        return self.marker.degrees == 0 ? 45 : 30
+    }
+    
+    private func capsuleColor() -> Color {
+        return self.marker.degrees == 0 ? .red : .gray
+    }
+    
+    private func textAngle() -> Angle {
+        return Angle(degrees: -self.compassDegrees - self.marker.degrees)
+    }
+    
 }

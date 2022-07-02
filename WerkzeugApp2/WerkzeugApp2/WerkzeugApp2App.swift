3af5e79edadba763391af2c47caef4b73b5c321d
Chiara Knipprath <chiara.knipprath@thm.iem.de>
2022-07-02 14:29:25 +0200

    Started new with SwiftUI

---
+++ WerkzeugApp2/WerkzeugApp2/WerkzeugApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  WerkzeugApp2App.swift
+//  WerkzeugApp2
+//
+//  Created by FMA2 on 02.07.22.
+//
+
+import SwiftUI
+
+@main
+struct WerkzeugApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
